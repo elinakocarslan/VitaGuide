4b8a690c0cebf5cdcbef0762438bf520ab3f738f
jonathandeng7 <jmdeng05@gmail.com>
2025-03-01 15:11:33 -0500

    minor intro page front end fixes

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -7,9 +7,12 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1 className={styles.title}>Welcome to VitaGuide</h1>
+        <h1 className={styles.title}>🌱 VitaCheck: Web App for Vitamin Diagnosis</h1>
         <p className={styles.description}>
-          Test your knowledge and learn about healthy living!
+          About Us:
+        </p>
+        <p className={styles.description}>
+        NutriMap is a mobile app that provides personalized nutrition insights, especially for underserved communities. It analyzes users’ symptoms and habits through a dynamic quiz, offering tailored nutrient recommendations and local food alternatives. With offline access and an SMS-based option, it remains accessible without internet. The app also helps NGOs track health trends using predictive analytics, enabling targeted interventions. Users can explore an interactive map for regional health insights and resources, while educational content fosters transparency and trust.
         </p>
         <Link href="/quiz" className={styles.startButton}>
           Start Quiz
