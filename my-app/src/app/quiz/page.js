782f0dc2608c771ddaf63fcd02217214a19c123c
jonathandeng7 <jmdeng05@gmail.com>
2025-03-01 14:50:19 -0500

    basic frameworks

---
+++ my-app/src/app/quiz/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import styles from '../page.module.css';
+
+export default function Quiz() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Quiz Page</h1>
+        <p>Quiz content will go here</p>
+      </div>
+    </main>
+  );
+} 
